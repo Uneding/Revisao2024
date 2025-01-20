effc8249bacc947cd76e3a2fb0620d89f92753e7
João Paulo <joaopaulo102003@gmail.com>
2025-01-20 11:01:37 -0300

    encerrar branch atual

--- Atividade_01/Program.cs
+++ Atividade_01/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using system;
+class Program
+{
+    
+    static void main
+    {
+        
+    }
+    static string Aleatorio (int tamanho, char deci)
+    {
+        string Cara_Senha;
+        if(desci.ToLower() == "s")
+        {
+            Cara_Senha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=<>?";
+        }else
+        {
+            Cara_Senha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        }
+        Random rand = new Random();
+        string senha = System.Text.StringBuilder(tamanho);
+        for (int i = 0; i < tamanho; i++)
+        {
+            char Cara_AL = Cara_Senha[rand.Next(Cara_Senha.Length)];
+            senha.Append(Cara_AL);
+        }
+
+        return senha.ToString();
+    }
+}
