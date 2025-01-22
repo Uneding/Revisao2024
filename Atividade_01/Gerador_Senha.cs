87477df171cd06cdd4964157886e8c3a618252df
João Paulo <joaopaulo102003@gmail.com>
2025-01-22 19:52:51 -0300

    Erro de andar no for antes de voltar no goto

--- Atividade_01/Gerador_Senha.cs
+++ Atividade_01/Gerador_Senha.cs
@@ -9,8 +9,9 @@ namespace Gerador_Senhas
 
             Random rand = new Random();
             StringBuilder senha = new System.Text.StringBuilder(tamanho);
-            int posi[] = new int[3];
-            int teste[] = { 1, 2, 3 };
+            int[] posi = new int[3];
+            int[] teste = { 1, 2, 3 };
+            int Num_Al = 0;
             for (int i = 0; i < 3; i++)
             {
                 if (i == 0)
@@ -23,18 +24,22 @@ namespace Gerador_Senhas
                     {
                         posi[i] = rand.Next(1, tamanho + 1);
 
-                    } while (posi[0] == posi[1] || posi[1 == posi[2]]);
+                    } while (posi[0] == posi[1] || posi[1] == posi[2] || posi[0] == posi[2]);
                 }
             }
 
             Array.Sort(posi);
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"{posi[i]} ");
+            }
             int x = 0;
 
             if (deci == 1)
             {
                 for (int i = 0; i < tamanho; i++)
                 {
-                    int Num_Al = rand.Next(48, 58);
+                    Num_Al = rand.Next(48, 58);
                     senha.Append((char)Num_Al);
                 }
             }
@@ -42,28 +47,25 @@ namespace Gerador_Senhas
             {
                 for (int i = 0; i < tamanho; i++)
                 {
-                retorno:
-                    int Num_Al = rand.Next(48, 91);
-                    if (Num_Al >= 58 && Num_Al <= 64)
-                    {
-                        goto retorno;
-                    }
-                    else if (x < 2 && i == posi[x])
-                    {
+                    Console.WriteLine($"-{i}");
                     retornodic2:
-                        int y = rand.Next(1, 3)
+                    if (x < 2 && i == posi[x])
+                    {
+                        
+                        int y = rand.Next(1, 3);
+                        Console.WriteLine(y);
                         if (y == 1)
                         {
                             if (teste[0] == 1)
                             {
-                                for (int i = 0; i < tamanho; i++)
-                                {
-                                    int Num_Al = rand.Next(48, 58);
-                                    senha.Append((char)Num_Al);
-                                }
-                                teste[0] == 0;
+                                Num_Al = rand.Next(48, 58);
+                                senha.Append((char)Num_Al);
+                                teste[0] = 0;
+                                Console.Write($"sim{(char)Num_Al}");
+                                Console.WriteLine(x);
+                                x++;
                             }
-                            if (teste[1] <> 0)
+                            if (teste[1] != 0)
                             {
                                 goto retornodic2;
                             }
@@ -72,57 +74,65 @@ namespace Gerador_Senhas
                         {
                             if (teste[1] == 2)
                             {
-                                for (int i = 0; i < tamanho; i++)
+                            retornodic3:
+                                Num_Al = rand.Next(65, 123);
+                                if (Num_Al >= 91 && Num_Al <= 96)
                                 {
-                                retornodic3:
-                                    int Num_Al = rand.Next(65, 123);
-                                    if (Num_Al >= 91 && Num_Al <= 96)
-                                    {
-                                        goto retornodic3;
-                                    }
-                                    teste[1] == 0;
+                                    goto retornodic3;
                                 }
-                                if (teste[0] <> 0)
+                                else
+                                {
+                                    senha.Append((char)Num_Al);
+                                    teste[1] = 0;
+                                    Console.Write($"sim{(char)Num_Al}");
+                                    Console.WriteLine(x);
+                                    x++;
+                                }
+
+                                if (teste[0] != 0)
                                 {
                                     goto retornodic2;
                                 }
                             }
 
                         }
-                        x++;
+                        
+                    
                     }
                     else
                     {
+                        retorno:
+                    Num_Al = rand.Next(48, 91);
+                    if (Num_Al >= 58 && Num_Al <= 64)
+                    {
+                        goto retorno;
+                    }
                         senha.Append((char)Num_Al);
                     }
                 }
             }
             else
             {
-                for (int i = 0; i < tamanho; i++)
+                for (int i5 = 0; i5 < tamanho; i5++)
                 {
-                retorno1:
-                    int Num_Al = rand.Next(33, 123);
-                    if (Num_Al == 46 || Num_Al == 44 || Num_Al == 59 || Num_Al == 58 || Num_Al == 92 || Num_Al == 93 || Num_Al == 94 || Num_Al == 91 || Num_Al == 47 || Num_Al == 39 || Num_Al == 34 || Num_Al == 96)
-                    {
-                        goto retorno1;
-                    }
-                    else if (x < 3 && i == posi[x])
-                    {
                     retornodic4:
-                        int y = rand.Next(1, 4)
+                    if (x < 3 && i5 == posi[x])
+                    {
+                        int y = rand.Next(1, 4);
+                        Console.WriteLine(y);
                         if (y == 1)
                         {
                             if (teste[0] == 1)
                             {
-                                for (int i = 0; i < tamanho; i++)
-                                {
-                                    int Num_Al = rand.Next(48, 58);
-                                    senha.Append((char)Num_Al);
-                                }
-                                teste[0] == 0;
+
+                                 Num_Al = rand.Next(48, 58);
+                                senha.Append((char)Num_Al);
+                                Console.Write($"sim{(char)Num_Al}");
+                                teste[0] = 0;
+                                Console.WriteLine(x);
+                                x++;
                             }
-                            if (teste[1] <> 0 || teste[2] <> 0)
+                            if (teste[1] != 0 || teste[2] != 0)
                             {
                                 goto retornodic4;
                             }
@@ -131,17 +141,22 @@ namespace Gerador_Senhas
                         {
                             if (teste[1] == 2)
                             {
-                                for (int i = 0; i < tamanho; i++)
+
+                            retornodic5:
+                                 Num_Al = rand.Next(65, 123);
+                                if (Num_Al >= 91 && Num_Al <= 96)
                                 {
-                                retornodic5:
-                                    int Num_Al = rand.Next(65, 123);
-                                    if (Num_Al >= 91 && Num_Al <= 96)
-                                    {
-                                        goto retornodic5;
-                                    }
-                                    teste[1] == 0;
+                                    goto retornodic5;
                                 }
-                                if (teste[0] <> 0 || teste[2] <> 0)
+                                else
+                                {
+                                    senha.Append((char)Num_Al);
+                                    teste[1] = 0;
+                                    Console.Write($"sim{(char)Num_Al}");
+                                    Console.WriteLine(x);
+                                    x++;
+                                }
+                                if (teste[0] != 0 || teste[2] != 0)
                                 {
                                     goto retornodic4;
                                 }
@@ -151,32 +166,43 @@ namespace Gerador_Senhas
                         {
                             if (teste[2] == 3)
                             {
-                                for (int i = 0; i < tamanho; i++)
+
+                            retornodic6:
+                                Num_Al = rand.Next(33, 96);
+                                if ((Num_Al >= 46 && Num_Al <= 58) || Num_Al == 44 || Num_Al == 92 || Num_Al == 93 || Num_Al == 94 || Num_Al == 91 || Num_Al == 39 || Num_Al == 34)
                                 {
-                                retornodic6:
-                                    int Num_Al = rand.Next(33, 96);
-                                    if ((Num_Al >= 46 && Num_Al <= 58)|| Num_Al == 44 || Num_Al == 92 || Num_Al == 93 || Num_Al == 94 || Num_Al == 91 || Num_Al == 39 || Num_Al == 34 )
-                                    {
-                                        goto retornodic6;
-                                    }
-                                    teste[2] == 0;
+                                    goto retornodic6;
                                 }
-                                if (teste[0] <> 0 || teste [1]<> 0)
+                                else
+                                {
+                                    senha.Append((char)Num_Al);
+                                    Console.Write($"sim{(char)Num_Al}");
+                                    Console.WriteLine(x);
+                                    teste[2] = 0;
+                                    x++;
+                                }
+                                if (teste[0] != 0 || teste[1] != 0)
                                 {
                                     goto retornodic4;
                                 }
                             }
                         }
-                        x++;
+                        
                     }
                     else
                     {
+                        retorno1:
+                    Num_Al = rand.Next(33, 123);
+                    if (Num_Al == 46 || Num_Al == 44 || Num_Al == 59 || Num_Al == 58 || Num_Al == 92 || Num_Al == 93 || Num_Al == 94 || Num_Al == 91 || Num_Al == 47 || Num_Al == 39 || Num_Al == 34 || Num_Al == 96)
+                    {
+                        goto retorno1;
+                    }
                         senha.Append((char)Num_Al);
                     }
-                    
+
                 }
             }
             return senha.ToString();
         }
     }
-}    
+}
